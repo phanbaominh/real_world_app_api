b2229e692133c6723131498a3662a6b38eb379c4
Minh Phan <pbminh2511@gmail.com>
2020-07-18 09:43:10 +0700

    Configure JBuilder to build lowerCamelCase key by default
    Enable controller to response to request with json by default and transform params key to snake_case

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
-# typed: strict
+# typed: false
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
   # All requests are authenticated using JWT tokens so just need to clear out session
   # instead of throwing exception when no CSRF token is detected
   protect_from_forgery with: :null_session
+
+  # Set default response to json
+  respond_to :json
+  before_action :underscore_params!
+
+  private
+
+  # transform all param key to underscore for easier handling
+  # due to API spec requiring json request's key be in camelCase
+  def underscore_params!
+    params.deep_transform_keys!(&:underscore)
+  end
 end
---
+++ config/initializers/jbuilder.rb
@@ -0,0 +1,5 @@
+# typed: strict
+# frozen_string_literal: true
+
+# Apply lowerCamelCase to all JSON key according to API spec
+Jbuilder.key_format camelize: :lower
