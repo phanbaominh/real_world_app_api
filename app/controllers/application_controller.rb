cb5748cff639b778e9761a39e57c3c126306d9ea
Minh Phan <pbminh2511@gmail.com>
2020-09-10 21:36:11 +0700

    Add allow origin to repsonse header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,16 @@ class ApplicationController < ActionController::Base
   before_action :underscore_params!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user_with_jwt
+  before_action :cors_set_controll_access_headers
 
   private
 
+  def cors_set_controll_access_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
   # transform all param key to underscore for easier handling
   # due to API spec requiring json request's key be in camelCase
   sig { void }
